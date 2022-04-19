96546e8b5507c21f41f1125cb8d79c7c208f3c51
João Nascimento <joaombn97@gmail.com>
2022-04-19 20:00:41 -0300

    Desired Style Chart Mock

--- src/Chart.js
+++ src/Chart.js
@@ -1,25 +1,71 @@
 import React from 'react';
-import './Chart.css';
+import {
+  CartesianGrid, Legend, Scatter, ScatterChart, Tooltip, XAxis, YAxis, ZAxis
+} from 'recharts';
 
-class Chart extends React.Component {
+const operationLine = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+];
+const equilibrium = [
+  { x: 1, y: 2 },
+  { x: 2, y: 5 },
+  { x: 3, y: 8 },
+  { x: 4, y: 10 },
+  { x: 5, y: 11 },
+];
+const stages = [
+  { x: 8, y: 8 },
+  { x: 3.5, y: 8 },
+  { x: 3.5, y: 3 },
+  { x: 1.5, y: 3 },
+  { x: 1.5, y: 1 },
+];
 
-  componentDidMount() {
-    const context = this.context;
-    context.beginPath();
-    context.arc(500, 200, 100, 0, 2 * Math.PI);
-    context.stroke();
-  }
+export default function Chart(props) {
 
-  render() {
-    return (
-      <div className="chart">
-        <canvas className='canvas' id='1' height='500' width='800'
-          ref={(context) => this.context = context?.getContext('2d')}
-        >
-        </canvas>
-      </div>
-    )
-  }
+  return (
+    <ScatterChart
+      width={500}
+      height={400}
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20
+      }}
+    >
+      <CartesianGrid />
+      <XAxis id='xAxis' type='number' dataKey='x' name='Aqueous Concentration' unit='g/L'
+        label={{ value: 'Aqueous Concentration', position: 'bottom' }}
+      />
+      <YAxis id='yAxis' type='number' dataKey='y' name='Organic Concentration' unit='g/L'
+        label={{ value: 'Organic Concentration', angle: -90, position: 'left' }}
+      />
+      <ZAxis type='number' range={[100]} />
+      <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+      <Legend verticalAlign='top' />
+      <Scatter
+        name='Operation Line'
+        data={operationLine}
+        fill='#8884d8'
+        line
+        shape='cross'
+      />
+      <Scatter
+        name='Equilibrium'
+        data={equilibrium}
+        fill='#82ca9d'
+        line
+        shape='diamond'
+      />
+      <Scatter
+        name='Stages'
+        data={stages}
+        fill='#303030'
+        line
+        shape='circle'
+      />
+    </ScatterChart>
+  );
 }
-
-export default Chart;
